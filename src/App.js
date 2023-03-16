253b87ca7f77370cbb811962dd3fe141ca2843f8
Sonu Singh <chandrabhanusonu@gmail.com>
2023-03-16 08:39:19 +0530

    Toggle Button

--- src/App.js
+++ src/App.js
@@ -3,25 +3,14 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-
-  const[data,setData] = useState(null);
-  const[print,setPrint] = useState(false);
-
-  function getData(val){
-    console.log(val.target.value);
-    setData(val.target.value);
-    setPrint(false);
-  }
-
+  const[status , setStatus] = useState(false);
   return (
-    <div className="App">      
-      {
-        print?
-        <h1>{data}</h1>
-        :null
-      }
-      <input type="text" onChange={getData}/>
-      <button onClick={() => setPrint(true)}>Print Data</button>
+    <div className="App">
+    {
+      status?
+      <h1>Hello World!</h1>:null
+    }      
+      <button onClick={() => setStatus(!status)}>Toggle</button>
     </div>
     
   );
