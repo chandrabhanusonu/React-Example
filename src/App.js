007468e5a8a5c33a340f4f42b9d2727f74aba39d
Sonu Singh <chandrabhanusonu@gmail.com>
2023-03-16 17:12:32 +0530

    Basic Form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 
+import { useState } from 'react';
 import './App.css';
-import React, { useState } from 'react';
 
 function App() {
-  const[status , setStatus] = useState(false);
+  const [name,setName] = useState("");
+  const [tac,setTac] = useState(false);
+  const[intrest,setIntrest] = useState("");
+  function getFormData(e) {
+    console.log(name,tac,intrest);
+    e.preventDefault();
+  }
   return (
     <div className="App">
-    {
-      status?
-      <h1>Hello World!</h1>:null
-    }      
-      <button onClick={() => setStatus(!status)}>Toggle</button>
+      <div>
+        <h1 style={{backgroundColor:"skyblue" , padding:"2rem"}}>Basic Form</h1>
+      </div>
+      <form onSubmit={getFormData}>
+        <input type="text" placeholder='Enter Name' onChange={(e) => setName(e.target.value)} /> <br /><br />
+        <select onChange={(e) => setIntrest(e.target.value)}>
+          <option>Select Option</option>
+          <option>Avanger</option>
+          <option>DC</option>
+        </select> <br /><br />
+        <input type="checkbox" onChange={(e) => setTac(e.target.checked)} /> <span>Accept Term and Condition</span> <br /><br />
+        <button type="submit">Submit</button> 
+      </form>
     </div>
     
   );
