7d5e606ad38cdeebcb7ca54ce5dc01a3d5077559
Sonu Singh <chandrabhanusonu@gmail.com>
2023-03-18 09:29:28 +0530

    Component Did Mount , Life Cycle Method

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 
 import './App.css';
-import User from './User';
 import React, { useState } from 'react';
-function App() {
-    const [name,setName] = useState("Sonu Singh");
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      name:"Sonu"
+    }
+    console.warn("constructor");
+  }
+
+  componentDidMount(){
+    console.warn("componentDidMount");
+  }
+
+  render(){
+    console.warn("render");
     return (
                                                                                                                                
       <div className="App">
         <div>
-          <h1>Render,Life Cycle Method</h1>
+          <h1>Component Did Mount , Life Cycle Method</h1>
         </div>
-        <User name={name} />
-        <button onClick={() => setName("Ram Singh")}>Update Name</button>
+        <h2>Name :- {this.state.name}</h2>
+        <button onClick={() => this.setState({name:"Ram"})}>update Name</button>
       </div>
       
     )
+  }
+    
   }
   
 export default App;
