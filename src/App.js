db9e0b68ab4728acc0888222a8020803b2a61925
Sonu Singh <chandrabhanusonu@gmail.com>
2023-03-18 15:21:40 +0530

    Should Component Update , Life Cycle Method

--- src/App.js
+++ src/App.js
@@ -6,23 +6,22 @@ class App extends React.Component {
 
   constructor(){
     super();
-    console.warn("constructor")
     this.state = {
       count:0
     }
   }
 
-  componentDidUpdate(preProps,perState,snapShort) {
-    console.warn("componentDidUpdate" , perState,snapShort)
-  }
-  
+ shouldComponentUpdate(){
+  console.warn("shouldComponentUpdate",this.state.count);
+  if(this.state.count <10)
+  return true;
+ }
 
   render(){
-    console.warn("render");     
     return (                                                                                                                        
       <div className="App">
         <div>
-          <h1>Component Did Update , Life Cycle Method</h1>
+          <h1>Should Component Update , Life Cycle Method</h1>
         </div>
         <h2>Number :- {this.state.count}</h2>
         <button onClick={()=> this.setState({count:this.state.count +1 })}>Update Number</button>
